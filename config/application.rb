31edc1e621d75605d8f28ff806177c2d35534290
wtsbee <atsushi@wataseatsushinoMacBook-Air.local>
2019-03-16 15:47:53 +0900

    Edit views
    ビューの修正

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module ChatSpace
       g.test_framework false
     end
     config.i18n.default_locale = :ja
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
   end
 end

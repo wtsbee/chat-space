a23c102f4589902abe117db5274031b6a5616d0e
wtsbee <atsushi@wataseatsushinoMacBook-Air.local>
2019-03-23 11:10:14 +0900

    Edit messages.rb

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
-  factory :image do
+  factory :message do
     content {Faker::Lorem.sentence}
-    image {File.open("#{Rails.root}//public/images/no_image.jpg")}
+    image {File.open("#{Rails.root}//public/uploads/message/image/3/スクリーンショット_2019-03-16_10.58.21.png")}
     user
     group
   end

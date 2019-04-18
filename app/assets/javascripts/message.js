7ba2f0530ec19c04466834602aab49ef0edefa06
wtsbee <atsushi@wataseatsushinoMacBook-Air.local>
2019-04-19 03:13:53 +0900

    Edit message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function() {
   function buildHTML(message){
     var content = message.content ? `${ message.content }` : "";
     var img = message.image ? `<img src= ${ message.image }>` : "";
-    var html = `<div class="message">
+    var html = `<div class="message" data-id="${message.id}">
                   <div class="message__upper-info">
                     <p class="message__upper-info__talker">
                       ${message.user_name}
@@ -20,6 +20,11 @@ $(function() {
                 </div>`
     return html;
   }
+
+  function scroll_view() {
+    $('.messages').animate({ scrollTop: $(".messages")[0].scrollHeight }, 500);
+  }
+
   $('#new').on('submit', function(e) {
     e.preventDefault();
     var formData = new FormData(this);
@@ -45,4 +50,40 @@ $(function() {
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
     })
   });
+
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    var last_message_id = $('.message').last().data('id');
+    var href = document.location.href;
+    var url = href.split('messages')[0] + "api/messages"
+    console.log(last_message_id);
+    if (href.match(/messages/)) {
+      $.ajax({
+        //ルーティングで設定した通りのURLを指定
+        url: url,
+        //ルーティングで設定した通りhttpメソッドをgetに指定
+        type: 'get',
+        dataType: 'json',
+        //dataオプションでリクエストに値を含める
+        data: {id: last_message_id},
+      })
+      .done(function(messages) {
+        //追加するHTMLの入れ物を作る
+        var insertHTML = '';
+        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        messages.forEach(function(message){
+          if (message.id > last_message_id){
+            insertHTML += buildHTML(message);
+            $('.messages').append(insertHTML);
+            scroll_view();
+          }
+        });
+      })
+      .fail(function() {
+        alert('自動更新に失敗しました');
+      });
+    }
+  };
+  setInterval(reloadMessages, 5000);
+
 });
